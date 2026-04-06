2c847b65d6a70c80e499f91bafc726d9b9f28720
Sarju S <sarju.s@sjcetpalai.ac.in>
2026-04-06 15:17:14 +0530

    new commit

---
+++ assignment.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def create_array():
+    pass
+
+def array_arithmetic(a, b):
+    pass
+
+def slicing_example(arr):
+    pass
+
+def matrix_multiplication(a, b):
+    pass
+
+def random_numbers():
+    pass
+
+def plot_graph():
+    pass
